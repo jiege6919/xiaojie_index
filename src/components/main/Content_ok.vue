396dc56b8f9eb438e06054327aef3ff4ec5e5364
jiege6919 <434871910@qq.com>
2024-08-29 18:08:33 +0800

    logo在晚上隐藏

--- src/components/main/Content_ok.vue
+++ src/components/main/Content_ok.vue
@@ -272,14 +272,15 @@
             <img id="skillPc" src="../../assets/images/skillPc.svg" alt="" srcset="">
             <img id="skillWap" src="../../assets/images/skillWap.svg" alt="" srcset="">
         </div>
-        <div class="title">
+        <div class="title dark_none">
             <svg class="icon" viewBox="0 0 20 20" height="26px" width="26px">
                 <path
                     d="M18.109,17.776l-3.082-3.081c-0.059-0.059-0.135-0.077-0.211-0.087c1.373-1.38,2.221-3.28,2.221-5.379c0-4.212-3.414-7.626-7.625-7.626c-4.212,0-7.626,3.414-7.626,7.626s3.414,7.627,7.626,7.627c1.918,0,3.665-0.713,5.004-1.882c0.006,0.085,0.033,0.17,0.098,0.234l3.082,3.081c0.143,0.142,0.371,0.142,0.514,0C18.25,18.148,18.25,17.918,18.109,17.776zM9.412,16.13c-3.811,0-6.9-3.089-6.9-6.9c0-3.81,3.089-6.899,6.9-6.899c3.811,0,6.901,3.09,6.901,6.899C16.312,13.041,13.223,16.13,9.412,16.13z">
                 </path>
             </svg> 小程序 / 公众号
         </div>
-        <div style="display: flex; flex-direction: column; justify-content: center; align-items: center; width: 80%;">
+        <div style="display: flex; flex-direction: column; justify-content: center; align-items: center; width: 80%;"
+            class="dark_none">
             <img src="/src/assets/images/logo.webp" alt="logo" title="logo" style="width: 84px;">
             <img class="xiaochengxu-light-dark-background" src="../../assets/images/1.png" alt="小程序">
         </div>
